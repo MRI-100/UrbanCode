f9327e616d1263e6e970435cee3d9c1bff883f9d
MRI-100 <mrityunjoykdeka100@gmail.com>
2026-05-25 20:00:41 +0530

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
@@ -22,6 +23,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
